6779672b656734aaaa76156d5d51d697c5e0fe82
B00143361 Amos Fakrogha <B00143361@mytudublin.ie>
2023-10-18 10:47:21 +0100

    booking showing up in database

--- createNewBooking.php
+++ createNewBooking.php
@@ -1,23 +1,19 @@
-<?php 
-
-$mid = $_POST['memberID']; 
-$cid = $_POST['courtID']; 
-$bd = $_POST['bookingDate']; 
-$st = $_POST['startTime']; 
-$et = $_POST['endTime']; 
+<?php
+$mid = $_POST['memberID'];
+$cid = $_POST['courtID'];
+$bd = $_POST['bookingDate'];
+$st = $_POST['startTime'];
+$et = $_POST['endTime'];
 
 include("dbcon.php");
 
-$sql = "insert into booking(memberid,courtid,bookingDate,starttime,endtime) values ('$mid','$cid','$bd','$st','$et')";
+$sql = "insert into booking(memberid, courtid, bookingDate, starttime, endtime) values ('$mid', '$cid', '$bd', '$st', '$et')";
 
-echo $sql;
-/*
 if (mysqli_query($conn, $sql)) {
-  echo "<br>New record created successfully";
+    echo "New record created successfully";
 } else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-*/
-
-mysqli_close($conn); 
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+} if memberid = 13 and courtid = 15 doesnt work check if these values exist in the database or if tehres any constyraints preventing it
+ 
+mysqli_close($conn);
 ?>
